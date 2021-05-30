43a483157544d62b7a393b5807c5056ee7dc6b82
Brandoneads <58013035+brandoneads@users.noreply.github.com>
2021-05-30 17:22:28 -0400

    first commit

---
+++ nav.js
@@ -0,0 +1,9 @@
+function navClick(){
+    var x = document.getElementById("navicon");
+    if(x.style.display === "block"){
+        x.style.display = "none";
+    }
+    else{
+        x.style.display = "block"
+    }
+}
